740d6b15e951a797b204b19379f8f105d9e3085c
mu7medashraf <150085237+mu7medashraf@users.noreply.github.com>
2025-09-12 18:35:52 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,87 +1,76 @@
 ﻿
-// Task Day 1:
-
-using System;
-
-namespace TaskDay1
+namespace TaskDay2
 {
+    struct time
+    {
+        public int hours;
+        public int minutes;
+        public int seconds;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Q(1): 
+            ///////////////////////////////////////////////////////////////
+            //Q(1):
+             Console.WriteLine("Enter number of students: ");
+             int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter a Character: ");
-            char input= Console.ReadLine()[0];
-            Console.WriteLine("The ASCII code of " + input + " is: " + (int)input);
+             string[] students = new string[n];
+             for (int i = 0; i < n; i++)
+             {
+                 Console.WriteLine("Enter name student " + (i + 1) + " : ");
+                 students[i] = Console.ReadLine();
+             }
+             Console.WriteLine("================= \nStudents names: ");
+             for (int i = 0; i < n; i++)
+             {
+                 Console.WriteLine(students[i]);
+             }
+            
+            ////////////////////////////////////////////////
+            //Q(2):
 
-            ////////////////////////////////////////////////////////////
-            // Q(2): 
+             Console.Write("===================\nEnter number of tracks: ");
+            int tracks = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter a number: ");
-            int input2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("The character for ASCII code "+input2 + " is: "+(char)input2);
-            //////////////////////////////////////////////////////////////////////////////////////////
-            // Q(3): 
-            Console.WriteLine("Enter number which you want to know it is odd or even: ");
-            int input3 =Convert.ToInt32(Console.ReadLine());
-            if(input3 % 2 == 0)
+            Console.Write("Enter number of students: ");
+            int student = Convert.ToInt32(Console.ReadLine());
+
+            int[,] ages = new int[tracks, student];
+
+            for (int i = 0; i < tracks; i++)
             {
-                Console.WriteLine("even");
+                Console.WriteLine("\n ====== Enter ages for track " +(i+1) + "=========" );
+                for (int j = 0; j < student; j++)
+                {
+                    Console.WriteLine("Enter age of student " + (j + 1) + " : ");
+                    ages[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
             }
-            else
+            Console.WriteLine("\n======== Student Ages & Averages ================ ");
+            for (int t = 0; t < tracks; t++)
             {
-                Console.WriteLine("odd");
+                int sum = 0;
+                Console.Write("Track "+ (t + 1)+ " ages: \n");
+                for (int s = 0; s < student; s++)
+                {
+                    Console.WriteLine(ages[t, s] + " ");
+                    sum += ages[t, s];
+                }
+                double average = (double)sum / student;
+                Console.WriteLine("Average Age = " + average+"\n");
             }
+            
 
-            /////////////////////////////////////////////////////////////////////////////////////////////////
-            // Q(4): 
-            Console.WriteLine("Enter number1: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter number2: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            int sum = num1 + num2;
-            Console.WriteLine("The sum of " + num1 + "+" + num2 + " = " + sum);
-
-            int sub = num1 - num2;
-            Console.WriteLine("The subtraction of " + num1 + "-" + num2 + " = " + sub);
-
-            int multi = num1 * num2;
-            Console.WriteLine("The multiplication of " + num1 + "*" + num2 + " = " + multi);
-
-            //////////////////////////////////////////////////////////////////////////////////////////
-            //Q(5):
-            Console.WriteLine("Enter youe degree of 0 to 100: ");
-            int deg = Convert.ToInt32(Console.ReadLine());
-
-            string grade;
-
-            if (deg >= 90)
-                grade = "A";
-            else if (deg >= 80)
-                grade = "B";
-            else if (deg >= 70)
-                grade = "C";
-            else if (deg >= 60)
-                grade = "D";
-            else
-                grade = "F";
-
-            Console.WriteLine("The grade is: " + grade);
-
-            /////////////////////////////////////////////////////////////////////////////////////////
-            //Q(6):
-            Console.Write("Enter a number to display its multiplication table: ");
-            int multi_table = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\nMultiplication Table for " + multi_table + ":");
-
-            for(int i=0; i <= 12; i++)
-            {
-                Console.WriteLine(multi_table + " x " + i + " = " + (multi_table * i));
-
-            }
-
-
+            /////////////////////////////////////////////////////////////
+            /// Q(3):
+            time t1;
+            Console.WriteLine("Enter Time: ");
+            t1.hours = Convert.ToInt32(Console.ReadLine());
+            t1.minutes = Convert.ToInt32(Console.ReadLine());
+            t1.seconds = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Time is: " + t1.hours + "H:" + t1.minutes + "M:" + t1.seconds + "S");
 
         }
     }
