d355a6bc5a74ecaf441e625d9ba21fe83f03796c
mu7medashraf <150085237+mu7medashraf@users.noreply.github.com>
2025-09-27 13:31:49 +0300

    Add files via upload

---
+++ Task_EntityFramework/Day 3/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_EF_day3.Models
+{
+    public class Student
+    { 
+        public int StudentId { get; set; }      
+        public string FullName { get; set; } = null!;
+        public int Age { get; set; }
+        public string? Email { get; set; }
+    }
+}
+
---
+++ Task_EntityFramework/Day 3/Models/UniversityContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_EF_day3.Models;
+
+namespace Task_EF_day3.Models
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() { }
+
+        public UniversityContext(DbContextOptions<UniversityContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Default: LocalDB. Replace SERVER_NAME if you want another server.
+                optionsBuilder.UseSqlServer("Server=MUHAMEDASHRAF;Database=University_CodeFirst;Trusted_Connection=True;Encrypt=False");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            { 
+                entity.HasKey(e => e.StudentId);
+                entity.Property(e => e.FullName).HasMaxLength(200).IsRequired();
+                entity.Property(e => e.Email).HasMaxLength(200);
+            });
+        }
+    }
+}
+
